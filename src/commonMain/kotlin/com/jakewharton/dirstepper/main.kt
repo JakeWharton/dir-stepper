dc025eaa8679ccd8051e6aedede5c705ba4b2284
Jake Wharton <jw@jakewharton.com>
2026-05-16 23:43:38 -0400

    Initial commit

---
+++ src/commonMain/kotlin/com/jakewharton/dirstepper/main.kt
@@ -0,0 +1,71 @@
+package com.jakewharton.dirstepper
+
+import kotlinx.io.buffered
+import kotlinx.io.files.FileSystem
+import kotlinx.io.files.Path
+import kotlinx.io.files.SystemFileSystem
+
+private const val STEP_FILE_PREFIX = ".step."
+
+fun main(vararg args: String) {
+	require(args.size == 4) {
+		"Wrong number of arguments: ${args.size}"
+	}
+	val (stepFileDirPath, stepDirPath, outDirPath, direction) = args
+
+	val stepFileDir = Path(stepFileDirPath)
+	val stepDir = Path(stepDirPath)
+	val outDir = Path(outDirPath)
+	val nextDelta = when (direction) {
+		"next" -> 1
+		"prev" -> -1
+		else -> throw UnsupportedOperationException("Unknown direction: $direction")
+	}
+
+	val fs = SystemFileSystem
+
+	val stepFile = fs.list(stepFileDir).single { it.name.startsWith(STEP_FILE_PREFIX) }
+	val currentStep = stepFile.name.let { name ->
+		check(name.startsWith(STEP_FILE_PREFIX)) {
+			"Unable to parse current page name: $name"
+		}
+		checkNotNull(name.substring(STEP_FILE_PREFIX.length).toIntOrNull()) {
+			"Unable to parse current page number in: $name"
+		}
+	}
+
+	val nextStep = currentStep + nextDelta
+	val nextStepDir = Path(stepDir, nextStep.toString())
+
+	fs.deleteRecursively(outDir)
+	fs.copyRecursively(nextStepDir, outDir)
+
+	fs.delete(stepFile)
+	fs.touch(Path(stepFileDir, STEP_FILE_PREFIX + nextStep))
+}
+
+private fun FileSystem.deleteRecursively(path: Path) {
+	if (metadataOrNull(path)!!.isDirectory) {
+		list(path).forEach(::deleteRecursively)
+	}
+	delete(path, mustExist = true)
+}
+
+private fun FileSystem.copyRecursively(source: Path, target: Path) {
+	if (metadataOrNull(source)!!.isDirectory) {
+		createDirectories(target, mustCreate = true)
+		list(source).forEach { child ->
+			copyRecursively(child, Path(target, child.name))
+		}
+	} else {
+		source(source).buffered().use { source ->
+			sink(target).use { sink ->
+				source.transferTo(sink)
+			}
+		}
+	}
+}
+
+private fun FileSystem.touch(path: Path) {
+	sink(path).close()
+}
